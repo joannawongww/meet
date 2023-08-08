a816157cb1fde3ceb13b7b4ba198e64b2a784a98
joannawongww <joanna_1994@hotmail.com>
2023-08-08 16:00:35 +0800

    fix serverless.yml typo

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const {google} = require("googleapis");
-const calendar = google.calendar('v3');
+const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ['https://joannawongww.github.io/meet'];
+const redirect_uris = ['https://joannawongww.github.io/meet/'];
 
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
