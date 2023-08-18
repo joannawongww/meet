8f89584b43fda158b7ec34e0fd553c20c73ce942
joannawongww <joanna_1994@hotmail.com>
2023-08-18 12:04:55 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -12,55 +12,59 @@ export const extractLocations = (events) => {
 // function check token's validity
 const checkToken = async (accessToken) => {
     const response = await fetch(
-        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
     );
     const result = await response.json();
     return result;
-}
+  };
 
 // remove code from URL
 const removeQuery = () => {
     let newurl;
     if (window.history.pushState && window.location.pathname) {
-        newurl = window.location.protol + "//" + window.location.host + window.location.pathname;
-        window.history.pushState("","", newurl);
+      newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname;
+      window.history.pushState("", "", newurl);
     } else {
-        newurl = window.location.protocol + "//" + window.location.host;
-        window.history.pushState("","", newurl);
+      newurl = window.location.protocol + "//" + window.location.host;
+      window.history.pushState("", "", newurl);
     }
-}
+  };
 
 // get new token - redirect user login with Google, redirect user to site with code
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
     const response = await fetch(
-        'https://jvzshwutu8.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+      'https://jvzshwutu8.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + '/' + encodeCode
     );
     const { access_token } = await response.json();
-    access_token && localStorage.setItem('access_token', access_token);
-
+    access_token && localStorage.setItem("access_token", access_token);
+  
     return access_token;
-}
+  };
 
 
 // fetch list of all events
 export const getEvents = async () => {
-    if (window.location.href.startsWith('https://localhost')) {
-       return mockData;
+    if (window.location.href.startsWith("http://localhost")) {
+      return mockData;
     }
-
-
+  
     const token = await getAccessToken();
+  
     if (token) {
-        removeQuery();
-        const url = "https://jvzshwutu8.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
-        const response = await fetch(url);
-        const result = await response.json();
-        if (result) {
-            return result.events;
-        } else return null;
+      removeQuery();
+      const url =  "https://jvzshwutu8.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+      const response = await fetch(url);
+      const result = await response.json();
+      if (result) {
+        return result.events;
+      } else return null; 
     }
-}
+  };
 
 //get access token
 export const getAccessToken = async () => {
