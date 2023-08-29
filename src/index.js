2c3293769151111496797f2ee234b1f84ded38e6
joannawongww <joanna_1994@hotmail.com>
2023-08-29 09:49:56 +0800

    integrate atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('a80db77c01b24cb5a72a7b4e1efef607').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
