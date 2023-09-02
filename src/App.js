5415c48a7f40d9dd86ccf47bb81f747f2a332cd6
joannawongww <joanna_1994@hotmail.com>
2023-09-02 15:25:07 +0800

    add InfoAlert

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch'
 import NumberOfEvents from './components/NumberOfEvents'
-import { extractLocations, getEvents } from './api'
+import { extractLocations, getEvents } from './api';
+import { InfoAlert } from './components/Alert';
 
 import './App.css';
 
@@ -11,7 +12,8 @@ function App() {
     const [events, setEvents] = useState([]);
     const [currentNOE, setCurrentNOE] = useState(32);
     const [allLocations, setAllLocations] = useState([]);
-    const [currentCity, setCurrentCity] = useState('See all cities')
+    const [currentCity, setCurrentCity] = useState('See all cities');
+    const [infoAlert, setInfoAlert] = useState("");
 
     
     useEffect(() => {
@@ -33,7 +35,10 @@ function App() {
   return (
     <div className="App">
        <h1 className="title">meet.</h1>
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+       <div className="alerts-container">
+            {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+       </div>
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} setInfoAlert={setInfoAlert} />
       <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
       <EventList events={events}/>
     </div>
